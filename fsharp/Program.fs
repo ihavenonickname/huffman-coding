36b6bd66e2f31ba68c8731c7038ef5c85665fc82
ihavenonickname <gbsm1993@gmail.com>
2017-06-08 01:07:43 -0300

    now it's working

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,44 +1,74 @@
 ﻿open System
 
-module PriorityQueue =
+module Huffman =
+    type private HuffmanNode =
+        | Internal of HuffmanNode * HuffmanNode * int
+        | Leaf of char * int
+
     let private count x xs =
         xs
         |> Seq.where (fun y -> y = x)
         |> Seq.length
 
-    let private normalize queue =
-        queue
-        |> List.sortBy (fun (_, freq) -> -freq)
+    let private weight = function
+        | Leaf (_, w) -> w
+        | Internal (_, _, w) -> w
 
-    let ofSeq xs =
-        xs
+    let private merge t1 t2 =
+        Internal (t1, t2, weight t1 + weight t2)
+
+    let private joinMaps =
+        Map.fold (fun acc key value -> Map.add key value acc)
+
+    let private freq text =
+        text
         |> Set.ofSeq
-        |> Set.map (fun c -> c, count c xs)
-        |> Set.toSeq
+        |> Set.map (fun c -> Leaf (c, count c text))
         |> List.ofSeq
-        |> normalize
 
-    let push x weight queue =
-        (x, weight) :: queue
-        |> normalize
+    let rec private huffman freqs =
+        match List.sortBy weight freqs with
+        | left :: right :: rest -> huffman (merge left right :: rest)
+        | [head] -> head
+        | [] -> failwith "Empty sequence"
 
-    let pop = function
-        | head :: xs -> Some head, xs
-        | [] -> None, []
+    let private codeTable tree =
+        let rec codeTable'  acc code tree =
+            match tree with
+            | Leaf (c, _) -> Map.add c code acc
+            | Internal (left, right, _) ->
+                let fromLeft = codeTable' acc (code + "0") left
+                let fromRight = codeTable' acc (code + "1") right
+                joinMaps fromLeft fromRight
+
+        match tree with
+        | Leaf (c, _) -> Map.ofList [(c, "0")]
+        | _ -> codeTable' Map.empty "" tree
+
+    let encode text =
+        let table =
+            text
+            |> freq
+            |> huffman
+            |> codeTable
+
+        text
+        |> Seq.map (fun c -> Map.find c table)
+        |> Seq.fold (+) ""
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let binary (n: int) =
+        Convert.ToString(n, 2).PadLeft(8, '0')
 
-    let queue = PriorityQueue.ofSeq "abbccc"
+    match List.ofArray argv with
+    | arg :: _ ->
+        arg
+        |> Seq.map (int >> binary)
+        |> Seq.fold (+) ""
+        |> printfn "Original:\n%s"
 
-    let rec f queue =
-        match PriorityQueue.pop queue with
-        | Some x, newQueue ->
-            printfn "%O" x
-            f newQueue
-        | _ -> ()
-
-    f queue |> ignore
+        printfn "Encoded:\n%s" (Huffman.encode arg)
+    | [] -> printfn "%s" "Cannot encode empty sequence"
 
     0
